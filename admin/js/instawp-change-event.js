3a4a69bd53de18ce8e8309a6e375646ceeef92c0
zilesingh1 <zilesecureweb@gmail.com>
2023-01-06 19:00:33 +0530

    selected events sync and custom post sync tested

--- admin/js/instawp-change-event.js
+++ admin/js/instawp-change-event.js
@@ -1,13 +1,72 @@
 jQuery(document).ready(function ($) {
+    //selected sync btn...
+    
+    
+    //bulk sync btn...
     $(document).on('click', '.bulk-sync-popup-btn', function(){
         $('.bulk-sync-popup').show();
+        $('.bulk-sync-popup').attr("data-sync-type", "bulk_sync");
+        $('.bulk-events-info').show();
+        $('.selected-events-info').hide();
     });
 
     $(document).on('click', '.bulk-sync-popup .close', function(){
         $('.bulk-sync-popup').hide();
     });
+    
+    //get Selected sync...
+   
+    jQuery('.sync_events_form tbody').on('click','input[type=checkbox]',function(){
+        var sync_selected_arr = [];
+        jQuery(".sync_events_form tbody input[type=checkbox]:checked").each(function() {
+             sync_selected_arr.push(jQuery(this).val());
+        });
+		if (sync_selected_arr.length > 0) {
+			jQuery('.selected-sync-popup-btn').show();
+		}else{
+			jQuery('.selected-sync-popup-btn').hide();
+		}
+        var sync_selected_str = sync_selected_arr.toString(); 
+        $('#selected_events').val(sync_selected_str);
+        var events_info = 'Total selected events: '+sync_selected_arr.length
+        $('.selected-events-info').html(events_info);
+        console.log(sync_selected_str);
 
-    //Single sync
+        // var event_slug = jQuery(this).parents('tr').find('.event_slug').text();
+	    // slug_arr.push(event_slug);
+    });
+
+    // slug_counts = {};
+    // jQuery.each(slug_arr, function(key,value) {
+    //     if (!slug_counts.hasOwnProperty(value)) {
+    //         slug_counts[value] = 1;
+    //     } else {
+    //         slug_counts[value]++;
+    //     }
+    // });
+
+    $(document).on('click', '.selected-sync-popup-btn', function(){
+        //console.log(slug_counts);
+        $('.bulk-sync-popup').show();
+        $('.bulk-sync-popup').attr("data-sync-type", "selected_sync");
+        $('.bulk-events-info').hide();
+        $('.selected-events-info').show();
+    });
+
+    //Bulk&Selected sync process...
+    $(document).on('click', '.sync-changes-btn', function(){
+        const sync_message = $("#sync_message").val();
+        const sync_type = $('.bulk-sync-popup').attr("data-sync-type");
+        var sync_ids = '';
+        if(sync_type == 'selected_sync'){
+            sync_ids = $('#selected_events').val();
+        }
+        //Initiate Step 2
+        $(".sync_process .step-1").removeClass('process_pending').addClass('process_inprogress');
+        packThings(sync_message,sync_type,sync_ids);
+    });
+
+    //Single sync..
     $(document).on('click', '.two-way-sync-btn', function(){
         var sync_ids = $(this).attr('data-id');
         const sync_message = 'This is a single sync.';
@@ -15,31 +74,6 @@ jQuery(document).ready(function ($) {
         $('#btn-sync-'+sync_ids).parent().find('.sync-loader').html('<img src="'+ajax_obj.plugin_images_url+'/loaders/loader.gif">');
         packThings(sync_message,'single_sync',sync_ids);
     });
-    // $(document).on('click', '.two-way-sync-btn', function(){
-    //     var sync_id = $(this).attr('data-id'); 
-    //     $.ajax({
-    //         url: ajax_obj.ajax_url, 
-    //         type: 'POST',
-    //         //dataType: "json",
-    //         data:{ 
-    //             action: 'single_sync', 
-    //             sync_id: sync_id
-    //         },
-    //         beforeSend: function() {
-    //             $('#btn-sync-'+sync_id).parent().find('.sync-loader').html('<img src="'+ajax_obj.plugin_images_url+'/loaders/loader.gif">');
-    //         },
-    //         success: function( response ){
-    //             //console.log(response);
-    //             if(response){
-    //                 $('.message-change-events').text(response);
-    //             }
-    //         },
-    //         complete: function(){
-    //             $('#btn-sync-'+sync_id).parent().find('.sync-loader').html('');
-    //             $('#btn-sync-'+sync_id).parent().find('.sync-success').show().delay(1000).fadeOut(300);
-    //         }
-    //     });
-    // });
 
     const baseCall = async (body) => {
         return await fetch(ajax_obj.ajax_url, {
@@ -56,12 +90,15 @@ jQuery(document).ready(function ($) {
         formData.append('sync_ids', sync_ids);
         formData.append('sync_message', sync_message);
         baseCall(formData).then((response) => response.json()).then((data) => {
+            console.log(data);
             if(data.success === true){
                 //Complete Step 1
                 $(".sync_process .step-1").removeClass('process_inprogress').addClass('process_complete');
                 //Initiate Step 2
                 $(".sync_process .step-2").removeClass('process_pending').addClass('process_inprogress');
                 bulkSync(sync_message,data.data,sync_type,sync_ids); 
+            }else{
+                $('.sync_error_success_msg').html('<p class="error">'+data.message+'</p>');  
             }
         }).catch((error) => {
             console.log("Error Occurred: ", error);
@@ -70,7 +107,7 @@ jQuery(document).ready(function ($) {
     
     const bulkSync = (sync_message, data, sync_type, sync_ids) => {
         let formData = new FormData();
-        formData.append('action', 'sync_changes'); //ajax action
+        formData.append('action', 'sync_changes'); //Ajax action
         formData.append('sync_message', sync_message);
         formData.append('sync_type', sync_type);
         formData.append('sync_ids', sync_ids);
@@ -111,15 +148,7 @@ jQuery(document).ready(function ($) {
                 }
             }
         });
-    }
-
-    //Bulk sync
-    $(document).on('click', '.sync-changes-btn', function(){
-        const sync_message = $("#sync_message").val();
-        //Initiate Step 2
-        $(".sync_process .step-1").removeClass('process_pending').addClass('process_inprogress');
-        packThings(sync_message,'bulk_sync','');
-    });
+    } 
 });
 
 
