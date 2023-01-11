fe269a829132804f61b5432ff41b73418098e832
Vikas Singhal <vikas.programmer@gmail.com>
2023-01-11 16:22:40 +0530

    0.0.9.1

--- instawp-connect.php
+++ instawp-connect.php
@@ -7,7 +7,7 @@
  * @wordpress-plugin
  * Plugin Name:       InstaWP Connect
  * Description:       Create 1-click staging, migration and manage your prod sites.
- * Version:           0.0.9
+ * Version:           0.0.9.1
  * Author:            InstaWP Team
  * Author URI:        https://instawp.com/
  * License:           GPL-3.0+
@@ -21,7 +21,7 @@ if ( ! defined( 'WPINC' ) ) {
     die;
 }
 
-define( 'INSTAWP_PLUGIN_VERSION', '0.0.9' );
+define( 'INSTAWP_PLUGIN_VERSION', '0.0.9.1' );
 //
 define('INSTAWP_RESTORE_INIT','init');
 define('INSTAWP_RESTORE_READY','ready');
