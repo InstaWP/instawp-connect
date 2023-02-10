ba49c0fa4200e71017bb5596eb0fd07bde529c36
Jaed <jaedm97@gmail.com>
2023-02-10 12:47:45 +0600

    check backup api

--- includes/class-instawp-rest-api.php
+++ includes/class-instawp-rest-api.php
@@ -1068,8 +1068,8 @@ class InstaWP_Backup_Api {
 		if ( $pre_backup['result'] == 'success' ) {
 
 			// Unique connection id / restore_id
-			$restore_id = $request->get_param( 'restore_id' );
-			$backup_now = $instawp_plugin->backup_now_api( $pre_backup['task_id'], $restore_id );
+//			$restore_id = $request->get_param( 'restore_id' );
+//			$backup_now = $instawp_plugin->backup_now_api( $pre_backup['task_id'], $restore_id );
 
 			$data     = array(
 				'task_id' => $pre_backup['task_id'],
