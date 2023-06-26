a93ef3a019cd7cb366f8fa5bfe0903822114981a
Jaed <jaedm97@gmail.com>
2023-06-26 16:56:17 +0600

    cli update

--- cli/class-instawp-cli.php
+++ cli/class-instawp-cli.php
@@ -16,9 +16,125 @@ if ( ! class_exists( 'INSTAWP_CLI_Commands' ) ) {
 		}
 
 
+		public function cli_restore( $migrate_task_id ) {
+
+			global $instawp_plugin, $InstaWP_Backup_Api;
+
+			if ( empty( $migrate_task = InstaWP_taskmanager::get_task( $migrate_task_id ) ) ) {
+				WP_CLI::error( esc_html__( 'Invalid task ID : ' . $migrate_task_id, 'instawp-connect' ) );
+
+				return false;
+			}
+
+			$migrate_task_data = InstaWP_Setting::get_args_option( 'data', $migrate_task );
+			$parameters        = InstaWP_Setting::get_args_option( 'parameters', $migrate_task_data );
+			$restore_options   = json_encode( array(
+				'skip_backup_old_site'     => '1',
+				'skip_backup_old_database' => '1',
+				'is_migrate'               => '1',
+				'backup_db',
+				'backup_themes',
+				'backup_plugin',
+				'backup_uploads',
+				'backup_content',
+				'backup_core',
+			) );
+			$backup_uploader   = new InstaWP_BackupUploader();
+			$backup_uploader->_rescan_local_folder_set_backup_api( $parameters );
+			$backup_list = InstaWP_Backuplist::get_backuplist();
+
+			if ( empty( $backup_list ) ) {
+				WP_CLI::error( esc_html__( 'Empty backup list.', 'instawp-connect' ) );
+
+				return false;
+			}
+
+			$backup_index      = 1;
+			$progress_response = [];
+
+			// before doing restore deactivate caching plugin
+			$instawp_plugin::disable_cache_elements_before_restore();
+
+			foreach ( $backup_list as $backup_list_key => $backup ) {
+				do {
+					$instawp_plugin->restore_api( $backup_list_key, $restore_options, $parameters );
+
+					$progress_results  = $instawp_plugin->get_restore_progress_api( $backup_list_key );
+					$progress_response = (array) json_decode( $progress_results );
+				} while ( $progress_response['status'] != 'completed' || $progress_response['status'] == 'error' );
+				$backup_index ++;
+			}
+
+			if ( $progress_response['status'] == 'completed' ) {
+
+				if ( isset( $parameters['wp'] ) && isset( $parameters['wp']['users'] ) ) {
+					$InstaWP_Backup_Api::create_user( $parameters['wp']['users'] );
+				}
+
+				if ( isset( $parameters['wp'] ) && isset( $parameters['wp']['options'] ) ) {
+					if ( is_array( $parameters['wp']['options'] ) ) {
+						$create_options = $parameters['wp']['options'];
+
+						foreach ( $create_options as $option_key => $option_value ) {
+							update_option( $option_key, $option_value );
+						}
+					}
+				}
+
+				$InstaWP_Backup_Api::write_htaccess_rule();
+
+				InstaWP_AJAX::instawp_folder_remover_handle();
+
+				// once the restore completed, enable caching elements
+				$instawp_plugin::enable_cache_elements_before_restore();
+			}
+
+			$instawp_plugin->delete_last_restore_data_api();
+			InstaWP_taskmanager::delete_task( $migrate_task_id );
+
+			WP_CLI::success( esc_html__( 'Restore started.', 'instawp-connect' ) );
+
+			return true;
+		}
+
+
+		public function cli_download( $migrate_task_id ) {
+
+			global $InstaWP_Curl;
+
+			if ( empty( $migrate_task = InstaWP_taskmanager::get_task( $migrate_task_id ) ) ) {
+				WP_CLI::error( esc_html__( 'Invalid task ID : ' . $migrate_task_id, 'instawp-connect' ) );
+
+				return false;
+			}
+
+			$migrate_task_data = InstaWP_Setting::get_args_option( 'data', $migrate_task );
+			$parameters        = InstaWP_Setting::get_args_option( 'parameters', $migrate_task_data );
+
+			if ( empty( $parameters ) && ! is_array( $parameters ) ) {
+				WP_CLI::error( esc_html__( 'No params for this task ID : ' . $migrate_task_id, 'instawp-connect' ) );
+
+				return false;
+			}
+
+			$download_response        = $InstaWP_Curl->download( $migrate_task_id, $parameters );
+			$download_response_result = InstaWP_Setting::get_args_option( 'result', $download_response );
+
+			if ( 'success' != $download_response_result ) {
+				WP_CLI::error( esc_html__( 'Could not download the backup files.', 'instawp-connect' ) );
+
+				return false;
+			}
+
+			WP_CLI::success( esc_html__( 'Backup downloaded successfully.', 'instawp-connect' ) );
+
+			return true;
+		}
+
+
 		function handle_instawp_commands( $args ) {
 
-			global $instawp_plugin, $InstaWP_Curl, $InstaWP_Backup_Api;
+			global $instawp_plugin;
 
 			$cli_action_index      = array_search( '-action', $args );
 			$cli_action            = $args[ ( $cli_action_index + 1 ) ] ?? '';
@@ -56,108 +172,14 @@ if ( ! class_exists( 'INSTAWP_CLI_Commands' ) ) {
 					break;
 
 				case 'upload';
-
+					// Upload
 					break;
 
 				case 'download';
 
-					if ( empty( $migrate_task = InstaWP_taskmanager::get_task( $migrate_task_id ) ) ) {
-						WP_CLI::error( esc_html__( 'Invalid task ID : ' . $migrate_task_id, 'instawp-connect' ) );
-						break;
-					}
+					$this->cli_download( $migrate_task_id );
 
-					$migrate_task_data = InstaWP_Setting::get_args_option( 'data', $migrate_task );
-					$parameters        = InstaWP_Setting::get_args_option( 'parameters', $migrate_task_data );
-
-					if ( empty( $parameters ) && ! is_array( $parameters ) ) {
-						WP_CLI::error( esc_html__( 'No params for this task ID : ' . $migrate_task_id, 'instawp-connect' ) );
-						break;
-					}
-
-					$download_response        = $InstaWP_Curl->download( $migrate_task_id, $parameters );
-					$download_response_result = InstaWP_Setting::get_args_option( 'result', $download_response );
-
-					if ( 'success' != $download_response_result ) {
-						WP_CLI::error( esc_html__( 'Could not download the backup files.', 'instawp-connect' ) );
-						break;
-					}
-
-					WP_CLI::success( esc_html__( 'Backup downloaded successfully.', 'instawp-connect' ) );
-
-//					break; 
-//				case 'restore':
-
-					if ( empty( $migrate_task = InstaWP_taskmanager::get_task( $migrate_task_id ) ) ) {
-						WP_CLI::error( esc_html__( 'Invalid task ID : ' . $migrate_task_id, 'instawp-connect' ) );
-						break;
-					}
-
-					$migrate_task_data = InstaWP_Setting::get_args_option( 'data', $migrate_task );
-					$parameters        = InstaWP_Setting::get_args_option( 'parameters', $migrate_task_data );
-					$restore_options   = json_encode( array(
-						'skip_backup_old_site'     => '1',
-						'skip_backup_old_database' => '1',
-						'is_migrate'               => '1',
-						'backup_db',
-						'backup_themes',
-						'backup_plugin',
-						'backup_uploads',
-						'backup_content',
-						'backup_core',
-					) );
-					$backup_uploader   = new InstaWP_BackupUploader();
-					$backup_uploader->_rescan_local_folder_set_backup_api( $parameters );
-					$backup_list = InstaWP_Backuplist::get_backuplist();
-
-					if ( empty( $backup_list ) ) {
-						WP_CLI::error( esc_html__( 'Empty backup list.', 'instawp-connect' ) );
-						break;
-					}
-
-					$backup_index      = 1;
-					$progress_response = [];
-
-					// before doing restore deactivate caching plugin
-					$instawp_plugin::disable_cache_elements_before_restore();
-
-					foreach ( $backup_list as $backup_list_key => $backup ) {
-						do {
-							$instawp_plugin->restore_api( $backup_list_key, $restore_options, $parameters );
-
-							$progress_results  = $instawp_plugin->get_restore_progress_api( $backup_list_key );
-							$progress_response = (array) json_decode( $progress_results );
-						} while ( $progress_response['status'] != 'completed' || $progress_response['status'] == 'error' );
-						$backup_index ++;
-					}
-
-					if ( $progress_response['status'] == 'completed' ) {
-
-						if ( isset( $parameters['wp'] ) && isset( $parameters['wp']['users'] ) ) {
-							$InstaWP_Backup_Api::create_user( $parameters['wp']['users'] );
-						}
-
-						if ( isset( $parameters['wp'] ) && isset( $parameters['wp']['options'] ) ) {
-							if ( is_array( $parameters['wp']['options'] ) ) {
-								$create_options = $parameters['wp']['options'];
-
-								foreach ( $create_options as $option_key => $option_value ) {
-									update_option( $option_key, $option_value );
-								}
-							}
-						}
-
-						$InstaWP_Backup_Api::write_htaccess_rule();
-
-						InstaWP_AJAX::instawp_folder_remover_handle();
-
-						// once the restore completed, enable caching elements
-						$instawp_plugin::enable_cache_elements_before_restore();
-					}
-
-					$instawp_plugin->delete_last_restore_data_api();
-					InstaWP_taskmanager::delete_task( $migrate_task_id );
-
-					WP_CLI::success( esc_html__( 'Restore started.', 'instawp-connect' ) );
+					$this->cli_restore( $migrate_task_id );
 
 					break;
 
