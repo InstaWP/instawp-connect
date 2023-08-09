37fc685205064569074a7596e1859ee56cab4100
Jaed Mosharraf <jaedm97@gmail.com>
2023-08-09 12:58:47 +0600

    curl update

--- includes/class-instawp-curl.php
+++ includes/class-instawp-curl.php
@@ -64,7 +64,7 @@ class InstaWP_Curl {
 				CURLOPT_RETURNTRANSFER => true,
 				CURLOPT_ENCODING       => '',
 				CURLOPT_MAXREDIRS      => 10,
-				CURLOPT_TIMEOUT        => 0,
+				CURLOPT_TIMEOUT        => 60,
 				CURLOPT_FOLLOWLOCATION => true,
 				CURLOPT_HTTP_VERSION   => CURL_HTTP_VERSION_1_1,
 				CURLOPT_CUSTOMREQUEST  => $api_method,
