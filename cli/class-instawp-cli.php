d9bc5c5c53090d1d5fd0ecef7194dacff3cb8664
Sayan Datta <hello@sayandatta.in>
2023-10-10 16:49:01 +0530

    added cli commands

--- cli/class-instawp-cli.php
+++ cli/class-instawp-cli.php
@@ -174,6 +174,37 @@ if ( ! class_exists( 'INSTAWP_CLI_Commands' ) ) {
 
 		function handle_instawp_commands( $args ) {
 
+			if ( isset( $args[0] ) && $args[0] === 'config-set' ) {
+				if ( isset( $args[1] ) ) {
+					if ( $args[1] === 'api-key' ) {
+						InstaWP_Setting::instawp_generate_api_key( $args[2], 'true' );
+					} else if ( $args[1] === 'api-domain' ) {
+						InstaWP_Setting::set_api_domain( $args[2] );
+					}
+				}
+				return true;
+			} 
+			
+			if ( isset( $args[0] ) && $args[0] === 'config-remove' ) {
+				$option = new \InstaWP\Connect\Helpers\Option();
+				$option->delete( [ 'instawp_api_key', 'instawp_api_options', 'instawp_connect_id_options' ] );
+
+				return true;
+			}
+
+			if ( isset( $args[0] ) && $args[0] === 'hard-reset' ) {
+				instawp_reset_running_migration( 'hard', false );
+
+				return true;
+			}
+
+			if ( isset( $args[0] ) && $args[0] === 'staging-set' && ! empty( $args[1] ) ) {
+				update_option( 'instawp_sync_connect_id', intval( $args[1] ) );
+				instawp_get_source_site_detail();
+
+				return true;
+			}
+
 			$cli_action_index      = array_search( '-action', $args );
 			$cli_action            = $args[ ( $cli_action_index + 1 ) ] ?? '';
 			$migrate_task_id_index = array_search( '-task', $args );
