ad3b92f2e9cd54a40ce6fa60915a83d18c29f13f
Jaed Mosharraf <jaedm97@gmail.com>
2023-11-09 14:54:30 +0600

    increase to next plugin version with dev label

--- instawp-connect.php
+++ instawp-connect.php
@@ -7,7 +7,7 @@
  * @wordpress-plugin
  * Plugin Name:       InstaWP Connect
  * Description:       Create 1-click staging, migration and manage your prod sites.
- * Version:           0.0.9.47
+ * Version:           0.0.9.48-dev
  * Author:            InstaWP Team
  * Author URI:        https://instawp.com/
  * License:           GPL-3.0+
@@ -23,7 +23,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 global $wpdb;
 
-define( 'INSTAWP_PLUGIN_VERSION', '0.0.9.47' );
+define( 'INSTAWP_PLUGIN_VERSION', '0.0.9.48' );
 define( 'INSTAWP_RESTORE_INIT', 'init' );
 define( 'INSTAWP_API_DOMAIN_PROD', 'https://app.instawp.io' );
 
