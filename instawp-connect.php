f8079b66f5e788b7ddf58b311573d76c61060165
Jaed Mosharraf <jaedm97@gmail.com>
2024-02-20 17:26:33 +0600

    release ready for 0.1.0.18

--- instawp-connect.php
+++ instawp-connect.php
@@ -7,7 +7,7 @@
  * @wordpress-plugin
  * Plugin Name:       InstaWP Connect
  * Description:       1-click WP Staging with Sync. Manage your Live sites.
- * Version:           0.1.0.17
+ * Version:           0.1.0.18
  * Author:            InstaWP Team
  * Author URI:        https://instawp.com/
  * License:           GPL-3.0+
@@ -23,7 +23,7 @@ if ( ! defined( 'WPINC' ) ) {
 
 global $wpdb;
 
-define( 'INSTAWP_PLUGIN_VERSION', '0.1.0.17' );
+define( 'INSTAWP_PLUGIN_VERSION', '0.1.0.18' );
 define( 'INSTAWP_API_DOMAIN_PROD', 'https://app.instawp.io' );
 
 $wp_plugin_url   = WP_PLUGIN_URL . '/' . plugin_basename( __DIR__ ) . '/';
