5ae5b404174774d8c2a08ec2fdb864716fdacc97
Jaed Mosharraf <jaedm97@gmail.com>
2024-02-21 18:11:40 +0600

    rename connect to connect with instawp

--- migrate/templates/part-create-connect.php
+++ migrate/templates/part-create-connect.php
@@ -14,7 +14,7 @@
         <div class="text-sm font-normal text-grayCust-50 mb-4"><?php esc_html_e( 'Please authorize your account in order to connect this site and enable staging site creation.', 'instawp-connect' ); ?></div>
         <a class="instawp-button-connect cursor-pointer	px-7 py-3 inline-flex items-center mx-auto rounded-md shadow-sm bg-primary-900 text-white hover:text-white active:text-white focus:text-white focus:shadow-none font-medium text-sm">
             <img src="<?php echo esc_url( instawp()::get_asset_url( 'migrate/assets/images/icon-plus.svg' ) ); ?>" class="mr-2" alt="">
-            <span><?php esc_html_e( 'Connect', 'instawp-connect' ); ?></span>
+            <span><?php esc_html_e( 'Connect with InstaWP', 'instawp-connect' ); ?></span>
         </a>
     </div>
 </div>
