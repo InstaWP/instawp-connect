9d8acdf6e27cef6225a903b3b1e69a0924c4517a
Sayan Datta <sdcreativity12@gmail.com>
2024-03-01 16:07:13 +0530

    added two blacklisted wp option

--- includes/sync/class-instawp-sync-option.php
+++ includes/sync/class-instawp-sync-option.php
@@ -73,7 +73,7 @@ class InstaWP_Sync_Option {
 	}
 
 	private function is_protected_option( $option ): bool {
-		$excluded_options = array( 'cron', 'instawp_api_options', 'siteurl', 'home', 'permalink_structure', 'rewrite_rules', 'recently_activated', 'active_plugins', 'theme_switched', 'sidebars_widgets', 'theme_switch_menu_locations' );
+		$excluded_options = array( 'cron', 'instawp_api_options', 'siteurl', 'home', 'permalink_structure', 'rewrite_rules', 'recently_activated', 'active_plugins', 'theme_switched', 'sidebars_widgets', 'theme_switch_menu_locations', 'recovery_mode_email_last_sent', 'recovery_keys' );
 
 		if ( in_array( $option, $excluded_options, true )
 			|| strpos( $option, '_transient' ) !== false
