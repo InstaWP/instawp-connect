98ac0747e7446be1c1d6eb1cf0cd0a57a0b2407e
Jaed Mosharraf <jaedm97@gmail.com>
2025-02-11 14:29:59 +0600

    fix directory issue

--- iwp-dest/index.php
+++ iwp-dest/index.php
@@ -2,7 +2,11 @@
 set_time_limit( 0 );
 error_reporting( 0 );
 
-include_once 'includes/functions-pull-push.php';
+if ( ! defined( 'IWP_PLUGIN_DIR' ) ) {
+	define( 'IWP_PLUGIN_DIR', dirname( dirname( __FILE__ ) ) . DIRECTORY_SEPARATOR );
+}
+
+include_once IWP_PLUGIN_DIR . 'includes' . DIRECTORY_SEPARATOR . 'functions-pull-push.php';
 
 if ( ! isset( $_SERVER['HTTP_X_IWP_MIGRATE_KEY'] ) || empty( $migrate_key = $_SERVER['HTTP_X_IWP_MIGRATE_KEY'] ) ) {
 	header( 'x-iwp-status: false' );
