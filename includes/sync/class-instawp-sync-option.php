4611e4eca9d4463a609481d2e98b109809b2b436
arpit <arpitsharmaind@gmail.com>
2026-01-19 12:15:57 +0530

    remove widget file and use option file and implement widget sync there

--- includes/sync/class-instawp-sync-option.php
+++ includes/sync/class-instawp-sync-option.php
@@ -7,17 +7,32 @@ defined( 'ABSPATH' ) || exit;
 class InstaWP_Sync_Option {
 
 	public $options_meta_name = 'instawp_2waysync_options_metadata';
+
+	/**
+	 * Flag to prevent recursive tracking during widget sync
+	 *
+	 * @var bool
+	 */
+	private $is_syncing = false;
+
 	public function __construct() {
-		// Update option
+		// Option hooks - also handles widget options
 		add_action( 'added_option', array( $this, 'added_option' ), 10, 2 );
 		add_action( 'updated_option', array( $this, 'updated_option' ), 10, 3 );
 		add_action( 'deleted_option', array( $this, 'deleted_option' ) );
 		add_action( 'init', array( $this, 'purge_instawp_option_cache' ) );
+
 		// Process event
 		add_filter( 'instawp/filters/2waysync/process_event', array( $this, 'parse_event' ), 10, 2 );
 	}
 
 	public function added_option( $option, $value ) {
+		// Handle widget options
+		if ( $this->is_widget_option( $option ) ) {
+			$this->track_widget_add( $option, $value );
+			return;
+		}
+
 		if ( ! InstaWP_Sync_Helpers::can_sync( 'option' ) ) {
 			return;
 		}
@@ -32,6 +47,18 @@ class InstaWP_Sync_Option {
 	}
 
 	public function updated_option( $option, $old_value, $value ) {
+		// Handle sidebars_widgets option
+		if ( 'sidebars_widgets' === $option ) {
+			$this->track_sidebars_widgets( $old_value, $value );
+			return;
+		}
+
+		// Handle widget options
+		if ( $this->is_widget_option( $option ) ) {
+			$this->track_widget_update( $option, $old_value, $value );
+			return;
+		}
+
 		if ( ! InstaWP_Sync_Helpers::can_sync( 'option' ) ) {
 			return;
 		}
@@ -84,6 +111,12 @@ class InstaWP_Sync_Option {
 	}
 
 	public function deleted_option( $option ) {
+		// Handle widget options
+		if ( $this->is_widget_option( $option ) ) {
+			$this->track_widget_delete( $option );
+			return;
+		}
+
 		if ( ! InstaWP_Sync_Helpers::can_sync( 'option' ) ) {
 			return;
 		}
@@ -93,9 +126,178 @@ class InstaWP_Sync_Option {
 		}
 	}
 
+	/**
+	 * Check if option is a widget option
+	 *
+	 * @param string $option_name Option name.
+	 * @return bool
+	 */
+	private function is_widget_option( $option_name ) {
+		return strpos( $option_name, 'widget_' ) === 0;
+	}
+
+	/**
+	 * Check if widget sync can proceed
+	 *
+	 * @return bool
+	 */
+	private function can_sync_widget() {
+		if ( $this->is_syncing ) {
+			return false;
+		}
+
+		return InstaWP_Sync_Helpers::can_sync( 'widget' );
+	}
+
+	/**
+	 * Generate a reference ID for widget options
+	 *
+	 * @param string $option_name Option name.
+	 * @return string
+	 */
+	private function get_widget_reference_id( $option_name ) {
+		return 'widget_' . md5( $option_name );
+	}
+
+	/**
+	 * Track sidebars_widgets option changes
+	 *
+	 * @param mixed $old_value Old value.
+	 * @param mixed $new_value New value.
+	 */
+	private function track_sidebars_widgets( $old_value, $new_value ) {
+		if ( ! $this->can_sync_widget() ) {
+			return;
+		}
+
+		// Don't track if values are the same
+		if ( $old_value === $new_value ) {
+			return;
+		}
+
+		// Skip if this is just array_version update
+		if ( is_array( $old_value ) && is_array( $new_value ) ) {
+			$old_copy = $old_value;
+			$new_copy = $new_value;
+			unset( $old_copy['array_version'], $new_copy['array_version'] );
+			if ( $old_copy === $new_copy ) {
+				return;
+			}
+		}
+
+		$reference_id = $this->get_widget_reference_id( 'sidebars_widgets' );
+
+		$data = array(
+			'name'  => 'sidebars_widgets',
+			'value' => maybe_serialize( $new_value ),
+		);
+
+		InstaWP_Sync_DB::insert_update_event(
+			__( 'Sidebars widgets updated', 'instawp-connect' ),
+			'update_option',
+			'widget',
+			$reference_id,
+			__( 'Sidebar Widgets', 'instawp-connect' ),
+			$data
+		);
+	}
+
+	/**
+	 * Track widget option add
+	 *
+	 * @param string $option_name Option name.
+	 * @param mixed  $value       Option value.
+	 */
+	private function track_widget_add( $option_name, $value ) {
+		if ( ! $this->can_sync_widget() ) {
+			return;
+		}
+
+		$reference_id = $this->get_widget_reference_id( $option_name );
+		$widget_name  = str_replace( 'widget_', '', $option_name );
+
+		$data = array(
+			'name'  => $option_name,
+			'value' => maybe_serialize( $value ),
+		);
+
+		InstaWP_Sync_DB::insert_update_event(
+			sprintf( __( 'Widget "%s" added', 'instawp-connect' ), $widget_name ),
+			'add_option',
+			'widget',
+			$reference_id,
+			sprintf( __( 'Widget: %s', 'instawp-connect' ), ucfirst( $widget_name ) ),
+			$data
+		);
+	}
+
+	/**
+	 * Track widget option update
+	 *
+	 * @param string $option_name Option name.
+	 * @param mixed  $old_value   Old value.
+	 * @param mixed  $new_value   New value.
+	 */
+	private function track_widget_update( $option_name, $old_value, $new_value ) {
+		if ( ! $this->can_sync_widget() ) {
+			return;
+		}
+
+		// Don't track if values are the same
+		if ( $old_value === $new_value ) {
+			return;
+		}
+
+		$reference_id = $this->get_widget_reference_id( $option_name );
+		$widget_name  = str_replace( 'widget_', '', $option_name );
+
+		$data = array(
+			'name'  => $option_name,
+			'value' => maybe_serialize( $new_value ),
+		);
+
+		InstaWP_Sync_DB::insert_update_event(
+			sprintf( __( 'Widget "%s" updated', 'instawp-connect' ), $widget_name ),
+			'update_option',
+			'widget',
+			$reference_id,
+			sprintf( __( 'Widget: %s', 'instawp-connect' ), ucfirst( $widget_name ) ),
+			$data
+		);
+	}
+
+	/**
+	 * Track widget option delete
+	 *
+	 * @param string $option_name Option name.
+	 */
+	private function track_widget_delete( $option_name ) {
+		if ( ! $this->can_sync_widget() ) {
+			return;
+		}
+
+		$reference_id = $this->get_widget_reference_id( $option_name );
+		$widget_name  = str_replace( 'widget_', '', $option_name );
+
+		InstaWP_Sync_DB::insert_update_event(
+			sprintf( __( 'Widget "%s" deleted', 'instawp-connect' ), $widget_name ),
+			'delete_option',
+			'widget',
+			$reference_id,
+			sprintf( __( 'Widget: %s', 'instawp-connect' ), ucfirst( $widget_name ) ),
+			$option_name
+		);
+	}
+
 	public function parse_event( $response, $v ) {
-		if ( $v->event_type !== 'option' ) {
+		// Only handle option and widget events
+		if ( ! in_array( $v->event_type, array( 'option', 'widget' ), true ) ) {
 			return $response;
+		}
+
+		// Set syncing flag for widgets to prevent recursive tracking
+		if ( $v->event_type === 'widget' ) {
+			$this->is_syncing = true;
 		}
 
 		$data = InstaWP_Sync_Helpers::object_to_array( $v->details );
@@ -110,17 +312,19 @@ class InstaWP_Sync_Option {
 			delete_option( $data );
 		}
 
+		// Reset syncing flag
+		$this->is_syncing = false;
+
 		return InstaWP_Sync_Helpers::sync_response( $v );
 	}
 
 	private function is_protected_option( $option ) {
-		$excluded_options = array( 'cron', 'instawp_api_options', 'siteurl', 'home', 'blog_public', 'permalink_structure', 'rewrite_rules', 'recently_activated', 'active_plugins', 'theme_switched', 'sidebars_widgets', 'theme_switch_menu_locations', 'recovery_mode_email_last_sent', 'recovery_keys', 'auto_updater.lock', 'elementor_version', 'elementor_log', 'iwp_connect_helper_error_log', 'iwp_failed_direct_process_media_events', 'iwp_sync_processed_media_ids', 'iwp_sync_config_data', 'iwp_mig_helper_error_log', 'instawp_last_heartbeat_data', $this->options_meta_name );
+		$excluded_options = array( 'cron', 'instawp_api_options', 'siteurl', 'home', 'blog_public', 'permalink_structure', 'rewrite_rules', 'recently_activated', 'active_plugins', 'theme_switched', 'theme_switch_menu_locations', 'recovery_mode_email_last_sent', 'recovery_keys', 'auto_updater.lock', 'elementor_version', 'elementor_log', 'iwp_connect_helper_error_log', 'iwp_failed_direct_process_media_events', 'iwp_sync_processed_media_ids', 'iwp_sync_config_data', 'iwp_mig_helper_error_log', 'instawp_last_heartbeat_data', $this->options_meta_name );
 
 		if ( in_array( $option, $excluded_options, true )
 			|| strpos( $option, '_transient' ) !== false
 			|| strpos( $option, 'instawp' ) !== false
 			|| strpos( $option, 'action_scheduler' ) !== false
-			|| strpos( $option, 'widget_' ) === 0 // Widget options are handled by InstaWP_Sync_Widget
 		) {
 			return true;
 		}
